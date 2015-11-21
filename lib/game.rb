c12f965bc941902cd9c25af560ccd3a2651b17e6
Phil Brockwell <nineteen40eight@gmail.com>
2015-11-21 12:52:48 +0000

    adds first test for game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,9 @@
 # class declaration for game class
 class Game
+
+	attr_reader :games
+
+	def initialize(frames)
+		@games = 1.upto(frames).map { |frame| Frame.new }
+	end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,9 @@
 require 'game'
 
 describe 'Game' do
+
+	it 'initialises with the correct amount of frames' do
+		test_game = Game.new(10)
+		expect(test_game.games.length).to eq(10)
+	end
 end
