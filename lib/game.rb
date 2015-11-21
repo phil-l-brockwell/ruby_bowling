51d379ac10dad541677c43082dc7d3c0e30570ed
Phil Brockwell <nineteen40eight@gmail.com>
2015-11-21 22:02:08 +0000

    refactors with rubocop

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,13 @@
 # class declaration for game class
 class Game
+  attr_reader :frames
 
-	attr_reader :frames
+  def initialize(frames)
+    @frames = {}
+    1.upto(frames).each { |num| @frames[num] = Frame.new(num) }
+  end
 
-	def initialize(frames)
-		@frames = {}
-		1.upto(frames).each { |num| @frames[num] = Frame.new(num) }
-	end
-
-	def current_frame
-		@frames.each { |key, frame| return frame unless frame.completed }
-	end
+  def current_frame
+    @frames.each { |_, frame| return frame unless frame.completed }
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,22 +1,21 @@
 require 'game'
 
 describe 'Game' do
+  let(:game) { Game.new(10) }
 
-	let(:game) { Game.new(10) }
+  context 'when initialised' do
+    it 'initialises with the correct amount of frames' do
+      expect(game.frames.length).to eq(10)
+    end
 
-	context 'when initialised' do
-		it 'initialises with the correct amount of frames' do
-			expect(game.frames.length).to eq(10)
-		end
+    it 'knows the current frame is 1' do
+      expect(game.current_frame.number).to eq(1)
+    end
+  end
 
-		it 'knows the current frame is 1' do
-			expect(game.current_frame.number).to eq(1)
-		end
-	end
-
-	context 'when bowling' do
-		it 'can check the score of a frame' do
-			expect(game.frames[1].score).to eq(0)
-		end
-	end
+  context 'when bowling' do
+    it 'can check the score of a frame' do
+      expect(game.frames[1].score).to eq(0)
+    end
+  end
 end
