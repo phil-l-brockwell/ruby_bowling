032d6ffe2abede10ddd4eb8afe04439d6e1fb5b6
Phil Brockwell <nineteen40eight@gmail.com>
2015-11-21 21:34:11 +0000

    adds test for current frame

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,15 @@ require 'game'
 
 describe 'Game' do
 
-	it 'initialises with the correct amount of frames' do
-		test_game = Game.new(10)
-		expect(test_game.games.length).to eq(10)
+	context 'when initialised' do
+		it 'initialises with the correct amount of frames' do
+			test_game = Game.new(10)
+			expect(test_game.frames.length).to eq(10)
+		end
+
+		it 'knows the current frame is 1' do
+			test_game = Game.new(10)
+			expect(test_game.current_frame.number).to eq(1)
+		end
 	end
 end
