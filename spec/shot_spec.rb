37846401fd7f2db38ce08fc2cecee8294def09b5
Phil Brockwell <nineteen40eight@gmail.com>
2015-11-23 00:30:16 +0000

    adds shot spec

---
+++ spec/shot_spec.rb
@@ -0,0 +1,24 @@
+require 'shot'
+
+describe 'Shot' do
+  let (:shot) { Shot.new }
+
+  context 'when initialised' do
+    it 'is not complete' do
+      expect(shot.complete).to eq(false)
+    end
+  end
+
+  context 'when bowling' do
+
+    before { shot.hit(1) }
+
+    it 'can update its score' do
+      expect(shot.score).to eq(1)
+    end
+
+    it 'is complete after a hit' do
+      expect(shot.complete).to eq(true)
+    end
+  end
+end
