3ffe2da471c6f4dba6a51777bea7100476ae599e
Phil Brockwell <nineteen40eight@gmail.com>
2015-12-04 19:28:34 +0000

    adds constants to frame class adds bonus methods

--- lib/frame.rb
+++ lib/frame.rb
@@ -1,27 +1,31 @@
 require 'shot'
 # class declaration for frame
 class Frame
-  attr_reader :number, :first_shot, :second_shot, :bonus
+  attr_reader :number, :first_shot, :second_shot, :bonus_score, :bonus_rolls
 
   PINS_IN_GAME = 10
+  BONUS_ROLL   = 1
+  STRIKE_BONUS = 2 * BONUS_ROLL
+  SPARE_BONUS  = 1 * BONUS_ROLL
 
   def initialize(number)
+    @number      = number
     @first_shot  = Shot.new
     @second_shot = Shot.new
-    @number      = number
-    @bonus       = 0
+    @bonus_score = @bonus_rolls = 0
   end
 
   def total
-    score + bonus
+    score + bonus_score
   end
 
   def score
     first_shot.score + second_shot.score
   end
 
-  def add_bonus(pins)
-    @bonus += pins
+  def bowl_bonus(pins)
+    @bonus_score += pins
+    @bonus_rolls -= BONUS_ROLL
   end
 
   def pins_remaining
@@ -40,12 +44,20 @@ class Frame
     first_shot.taken && second_shot.taken || strike?
   end
 
+  def bowl(pins)
+    fail IllegalShotError if pins > pins_remaining
+    current_shot.knock_over pins
+    calc_bonus if complete?
+  end
+
+  private
+  
   def current_shot
     first_shot.taken ? second_shot : first_shot
   end
 
-  def bowl(pins)
-    fail IllegalShotError if pins > pins_remaining
-    current_shot.knock_over pins
+  def calc_bonus
+    @bonus_rolls += STRIKE_BONUS if strike?
+    @bonus_rolls += SPARE_BONUS  if spare?
   end
 end
