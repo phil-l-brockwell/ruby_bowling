d6250e053a0dbf01045aa297b7839bd71f1ac90c
Phil Brockwell <nineteen40eight@gmail.com>
2015-12-04 19:30:44 +0000

    adds empty frame constant, refactors build frames to one line, refactors bowl method to use bonus_rolls attr

--- lib/game.rb
+++ lib/game.rb
@@ -2,26 +2,26 @@
 class Game
   attr_reader :frames
 
+  EMPTY_FRAME = Frame.new(0)
+
   def initialize(frames = 10)
-    @frames = {}
-    1.upto(frames).each { |num| @frames[num] = Frame.new(num) }
+    @frames = Hash[(1..frames).map { |x| [x, Frame.new(x)] }]
   end
 
-  def previous_frame
-    @frames[current_frame.number - 1]
+  def pre_frame
+    frames[current_frame.number - 1] || EMPTY_FRAME
   end
 
   def current_frame
-    @frames.each { |_, frame| return frame unless frame.complete? }
+    frames.each { |_, frame| return frame unless frame.complete? }
   end
 
   def total
-    @frames.values.inject(0) { |a, frame| a + frame.score }
+    frames.values.inject(0) { |a, frame| a + frame.total }
   end
 
   def bowl(pins)
-    previous_frame.add_bonus(pins) if previous_frame && previous_frame.spare? && !current_frame.first_shot.taken
-    previous_frame.add_bonus(pins) if previous_frame && previous_frame.strike?
+    pre_frame.bowl_bonus pins if pre_frame.bonus_rolls > 0
     current_frame.bowl pins
   end
 end
