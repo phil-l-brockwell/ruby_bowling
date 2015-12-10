05bad039ca9c7289511af321aa90a2d5fffd563b
Phil Brockwell <nineteen40eight@gmail.com>
2015-12-10 13:34:53 +0000

    fixes indentation

--- lib/shot.rb
+++ lib/shot.rb
@@ -1,14 +1,14 @@
 # Defines the shot class
 class Shot
-  attr_reader :taken, :score
+  attr_reader :taken, :pins_remaining, :score
 
   def initialize
-    @score = 0
     @taken = false
+    @score = 0
   end
 
   def knock_over(pins)
-    @score = pins
+    @score += pins
     @taken = true
   end
 end
