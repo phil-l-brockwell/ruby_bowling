9fb4acaca9bf15cf3bdb2e1e1f38124b299bbcd2
Phil Brockwell <nineteen40eight@gmail.com>
2015-12-30 12:08:51 +0000

    adds tests for iterator

--- spec/shot_holder_spec.rb
+++ spec/shot_holder_spec.rb
@@ -2,5 +2,14 @@ require 'shot_holder'
 
 describe 'ShotHolder' do
   context 'when initialised' do
+    it 'has an iterator' do
+      test_holder = ShotHolder.new
+      expect(test_holder).to respond_to(:iterator)
+    end
+
+    it 'has the iterator set at 1' do
+      test_holder = ShotHolder.new
+      expect(test_holder.iterator).to eq(1)
+    end
   end
 end
