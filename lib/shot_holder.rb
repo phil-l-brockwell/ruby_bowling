41d7c30f818e5ce140799a1abbe7a4cb12dbce0d
Phil Brockwell <nineteen40eight@gmail.com>
2016-01-05 21:41:07 +0000

    adds default arg to initialise method so can be initialised with shots or without

--- lib/shot_holder.rb
+++ lib/shot_holder.rb
@@ -1,8 +1,20 @@
 # class declaration for shot holder
 class ShotHolder
-  attr_accessor :iterator
+  attr_accessor :iterator, :shots
 
-  def initialize
-    @iterator = 1
+  def initialize(shots = 0)
+    @iterator = shots + 1
+    @shots = Hash[(1..shots).map { |x| [x, Shot.new] }] || {}
+  end
+
+  def add_shot
+    shots[iterator] = Shot.new
+    increment_iterator
+  end
+
+  private
+
+  def increment_iterator
+    @iterator += 1
   end
 end
--- spec/shot_holder_spec.rb
+++ spec/shot_holder_spec.rb
@@ -1,15 +1,46 @@
 require 'shot_holder'
 
 describe 'ShotHolder' do
+  let(:test_holder) { ShotHolder.new }
+
   context 'when initialised' do
     it 'has an iterator' do
-      test_holder = ShotHolder.new
       expect(test_holder).to respond_to(:iterator)
     end
 
     it 'has the iterator set at 1' do
-      test_holder = ShotHolder.new
       expect(test_holder.iterator).to eq(1)
+    end
+
+    it 'has a hash for shots' do
+      expect(test_holder).to respond_to(:shots)
+    end
+
+    it 'is initialised with 0 shots' do
+      expect(test_holder.shots.length).to eq(0)
+    end
+
+    it 'can be initialised with shots' do
+      test_holder_with_shots = ShotHolder.new(2)
+      expect(test_holder_with_shots.shots.length).to eq(2)
+    end
+  end
+
+  context 'when adding a shot' do
+    it 'adds the new shot to the hash' do
+      expect { test_holder.add_shot }
+        .to change { test_holder.shots.length }.from(0).to(1)
+    end
+
+    it 'increases the iterator' do
+      expect { test_holder.add_shot }
+        .to change { test_holder.iterator }.from(1).to(2)
+    end
+  end
+
+  context 'when bowling' do
+    it 'knows if all the shots are taken' do
+      
     end
   end
 end
